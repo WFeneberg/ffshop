20ca430d12993220cdae45f5d259c5e55b193a90
Wolfgang Feneberg <WFeneberg@yahoo.de>
2024-06-20 19:43:34 +0200

    Benutzer registrieren

--- register.php
+++ register.php
@@ -1,43 +1,16 @@
 <?php
-// Verbindung zur Datenbank herstellen
-$servername = "localhost";
-$username = "db_user";
-$password = "db_password";
-$dbname = "meine_datenbank";
+$pdo = new PDO('mysql:host=localhost;dbname=ffshop', 'root', 'root'); // Verbindung zur Datenbank
 
-// Erstellen der Verbindung
-$conn = new mysqli($servername, $username, $password, $dbname);
+if (isset($_POST['username'], $_POST['password'], $_POST['email'])) {
 
-// Überprüfen der Verbindung
-if ($conn->connect_error) {
-    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+$stmt = $pdo->prepare("INSERT INTO users (username, password, email) VALUES (:username, :password, :email)");
+$stmt->execute([
+    ':username' => $_POST['username'],
+    ':password' => password_hash($_POST['password'], PASSWORD_DEFAULT),
+    ':email' => $_POST['email']
+    ]);
+
+    echo "User hinzugefügt.";
 }
 
-// Daten aus dem Post-Request holen
-$user = $_POST['username'];
-$email = $_POST['email'];
-$pass = $_POST['password'];
-
-// Passwort verschlüsseln
-$hashed_password = password_hash($pass, PASSWORD_DEFAULT);
-
-// SQL zum Einfügen des neuen Benutzers
-$sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
-
-// Vorbereiten des SQL-Statements
-$stmt = $conn->prepare($sql);
-
-// Parameter binden
-$stmt->bind_param("sss", $user, $hashed_password, $email);
-
-// Statement ausführen
-if ($stmt->execute()) {
-    echo "Registrierung erfolgreich!";
-} else {
-    echo "Fehler: " . $sql . "<br>" . $conn->error;
-}
-
-// Verbindung schließen
-$stmt->close();
-$conn->close();
 ?>
