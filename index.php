fc766ad55816e454c9b0b6259c4d4a84cd394973
Wolfgang Feneberg <WFeneberg@yahoo.de>
2024-06-23 18:20:52 +0200

    fix delete

--- index.php
+++ index.php
@@ -9,7 +9,9 @@ $pdo = new PDO('mysql:host=localhost;dbname=warehousedb', 'root', 'root');
 
 $artikel = $pdo->query("SELECT * FROM lager")->fetchAll();
 
-$warenkorb = $pdo->query("SELECT * FROM warenkorb")->fetchAll();
+$warenkorb = $pdo->query("SELECT warenkorb.*, lager.Produkt, lager.Preis, warenkorb.Anzahl * lager.Preis AS Gesamtpreis
+                          FROM warenkorb
+                          JOIN lager ON warenkorb.ProduktID = lager.ProduktID;")->fetchAll();
 
 $payments = $pdo->query("SELECT * FROM zahlung")->fetchAll();
 
@@ -112,6 +114,27 @@ if (isset($_POST['aktion']) && $_POST['aktion'] === 'order') {
   $pay_id = $_POST['pay_id'];
 
   saveToCart($kunden_id, $produktID, $pay_id);
+  
+  $Meldungen = "Produkt wurde zum Warenkorb hinzugefügt.";
+  
+}
+
+function deletefromCart($kundenID, $produktID) {
+  $pdo = new PDO('mysql:host=localhost;dbname=warehousedb', 'root', 'root');
+// Verbindung zur Datenbank
+  // Überprüfen, ob das Produkt bereits im Warenkorb des Benutzers ist
+  $stmt = $pdo->prepare("DELETE FROM warenkorb WHERE KundenID = :KundenID AND ProduktID = :produktID");
+  $stmt->execute(['KundenID' => $kundenID, 'produktID' => $produktID]);
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+}
+
+if (isset($_POST['aktion']) && $_POST['aktion'] === 'delete') {
+  
+  $produktID = $_POST['ProduktID'];
+  $kunden_id = $_POST['kunden_id'];
+
+  deletefromCart($kunden_id, $produktID);
   
   $Meldungen = "Produkt wurde zum Warenkorb hinzugefügt.";
   
@@ -234,16 +257,23 @@ if (isset($_POST['aktion']) && $_POST['aktion'] === 'order') {
     <table>
         <tr>
             <th>Kunde</th>
+            <th>ProduktID</th>
             <th>Produkt</th>
             <th>Zahlungsart</th>
             <th>Anzahl</th>
+            <th>Preis</th>
+            <th>Gesamtpreis</th>
         </tr>
         <?php foreach ($warenkorb as $a): ?>
+          <?php if ($a['KundenID'] == $kunden_id): ?>
         <tr>
             <td><?= htmlspecialchars($a['KundenID']) ?></td>
             <td><?= htmlspecialchars($a['ProduktID']) ?></td>
             <td><?= htmlspecialchars($a['PayID']) ?></td>
+            <td><?= htmlspecialchars($a['Produkt']) ?></td>
             <td><?= htmlspecialchars($a['Anzahl']) ?></td>
+            <td><?= htmlspecialchars($a['Preis']) ?></td>
+            <td><?= htmlspecialchars($a['Gesamtpreis']) ?></td>
             <td>
                 <form action="" method="post">
                     <input type="hidden" name="aktion" value="delete">
@@ -254,6 +284,7 @@ if (isset($_POST['aktion']) && $_POST['aktion'] === 'order') {
                 </form>
             </td>
         </tr>
+        <?php endif; ?>
         <?php endforeach; ?>
     </table>
     </div>
